3d72b82cdbdd393020c64c3d6bcbde798314e931
Dylan <63360083+czyrok@users.noreply.github.com>
2023-11-07 10:45:49 +0100

    Modification du SocketController

--- src/socket/socketController.ts
+++ src/socket/socketController.ts
@@ -1,26 +1,43 @@
-import type { Database } from "../database/database";
-import { Server } from "socket.io";
+import { Database } from "../database/Database";
+import { Server, Socket } from "socket.io";
+import { TokenHelper } from "../helpers/TokenHelper";
+import { IUser } from "../database/models/UserModel";
+import { IConversation } from "../database/models/ConversationModel";
 
-export class SocketController
-{
-	/*
+export class SocketController {
+  /*
 		Pour savoir si un utilisateur est connecté depuis la route /online,
 		Nous devons stocker une correspondance socketId <=> userId.
 	*/
 
-	constructor(private io:Server, private Database:Database)
-	{
-		this.connect();
-		this.listenRoomChanged();
-	}
+  public readonly socketIdUserIdMap = new Map<string, string>();
 
-	connect()
-	{
-		this.io.on("connection", (socket) => {
-			// Récupérer les infos voulu depuis les extra headers.
-			// socket.handshake.headers contient ce que vous voulez. 
+  public constructor(
+    private readonly io: Server,
+    private readonly database: Database
+  ) {
+    this.connect();
+    this.listenRoomChanged();
+  }
 
-			/*
+  connect() {
+    this.io.on("connection", async (connectedSocket: Socket) => {
+      const currentUser = await this.getSocketUser(connectedSocket);
+
+      if (!currentUser) return;
+
+      this.linkSocketIdUserId(connectedSocket, currentUser.id);
+
+      const userConversations: Array<IConversation> =
+        await this.database.getAllConversationsForUser(currentUser);
+
+      for (const conversation of userConversations) {
+        connectedSocket.join(conversation.id);
+      }
+
+      // Récupérer les infos voulu depuis les extra headers.
+      // socket.handshake.headers contient ce que vous voulez.
+      /*
 				Dès qu'un socket utilisateur arrive, on veut l'ajouter à la room
 				pour chaque conversation dans laquelle il se trouve. 
 
@@ -35,28 +52,60 @@ export class SocketController
 					Le paramètre roomName doit absolument être de type string,
 					si vous mettez un type number, cela ne fonctionnera pas.
 			*/
-		});
-	}
+    });
+  }
 
-	// Cette fonction vous sert juste de debug.
-	// Elle permet de log l'informations pour chaque changement d'une room. 
-	listenRoomChanged()
-	{
-		this.io.of("/").adapter.on("create-room", (room) => {
-			console.log(`room ${room} was created`);
-		});
+  private getUserId(connectedSocket: Socket): string | undefined {
+    const userToken: string | undefined =
+      connectedSocket.handshake.headers.authorization;
 
-		this.io.of("/").adapter.on("join-room", (room, id) => {
-			console.log(`socket ${id} has joined room ${room}`);
-		});
+    if (!userToken) return;
 
-		this.io.of("/").adapter.on("leave-room", (room, id) => {
-			console.log(`socket ${id} has leave room ${room}`);
-		});
+    let userTokenPayload = TokenHelper.decodeUserToken(userToken);
 
-		this.io.of("/").adapter.on("delete-room", (room) => {
-			console.log(`room ${room} was deleted`);
-		});
-	}
+    if (!userTokenPayload.sub || typeof userTokenPayload.sub !== "string")
+      return undefined;
+
+    return userTokenPayload.sub;
+  }
+
+  private linkSocketIdUserId(socket: Socket, userId: string): void {
+    this.socketIdUserIdMap.set(socket.id, userId);
+
+    socket.on("disconnect", () => {
+      this.socketIdUserIdMap.delete(socket.id);
+    });
+  }
+
+  private async getSocketUser(socket: Socket): Promise<IUser | undefined> {
+    const currentUserId: string | undefined = this.getUserId(socket);
+
+    if (!currentUserId) return undefined;
+
+    const currentUser: IUser | null = await this.database.getUserById(
+      currentUserId
+    );
+
+    return currentUser ?? undefined;
+  }
+
+  // Cette fonction vous sert juste de debug.
+  // Elle permet de log l'informations pour chaque changement d'une room.
+  listenRoomChanged() {
+    this.io.of("/").adapter.on("create-room", (room) => {
+      console.log(`room ${room} was created`);
+    });
+
+    this.io.of("/").adapter.on("join-room", (room, id) => {
+      console.log(`socket ${id} has joined room ${room}`);
+    });
+
+    this.io.of("/").adapter.on("leave-room", (room, id) => {
+      console.log(`socket ${id} has leave room ${room}`);
+    });
+
+    this.io.of("/").adapter.on("delete-room", (room) => {
+      console.log(`room ${room} was deleted`);
+    });
+  }
 }
-
