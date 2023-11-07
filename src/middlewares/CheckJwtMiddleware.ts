0e0e8359989b15cbc620dd7594c7cbb7463f82f9
Dylan <63360083+czyrok@users.noreply.github.com>
2023-11-07 10:03:22 +0100

    Ajout du middleware CheckJwtMiddleware

---
+++ src/middlewares/CheckJwtMiddleware.ts
@@ -0,0 +1,44 @@
+import { NextFunction, Request, Response } from "express";
+import {
+  Code400HttpError,
+  Code401HttpError,
+  HttpError,
+} from "../error/HttpError";
+import { IUser } from "../database/models/UserModel";
+import { TokenHelper } from "../helpers/TokenHelper";
+
+export async function checkJwtMiddleware(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> {
+  const userToken: string | undefined = request.headers.authorization;
+
+  try {
+    if (!userToken) throw new Code401HttpError("You need a token");
+
+    let userTokenPayload = TokenHelper.decodeUserToken(userToken);
+
+    if (!userTokenPayload.sub || typeof userTokenPayload.sub !== "string")
+      throw new Code400HttpError("The token contains an invalid user id");
+
+    const database = request.app.locals.database;
+
+    const currentUser: IUser | null = await database.getUserById(
+      userTokenPayload.sub
+    );
+
+    if (!currentUser)
+      throw new Code400HttpError("The token contains an invalid user id");
+
+    request.currentUser = currentUser;
+
+    next();
+  } catch (error: unknown) {
+    const httpError: HttpError = HttpError.handleUnknownError(error);
+
+    response.status(httpError.status).send({
+      error: httpError.message,
+    });
+  }
+}
