7a2d47f7c48e192ea2f1f9c9da611a4605a22109
Dylan <63360083+czyrok@users.noreply.github.com>
2023-11-07 10:46:20 +0100

    Ajout de la propriété currentUser pour les requêtes Express

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,17 +2,22 @@ import mongoose, { Schema } from "mongoose";
 import { Database } from "./database/database";
 import { SocketController } from "./socket/socketController";
 import { ConversationController } from "./database/Mongo/Controllers/conversationController";
+import { IUser } from "./database/models/UserModel";
 
 export type MongooseID = Schema.Types.ObjectId | string | null;
 
 declare global {
-	namespace Express {
-		interface Locals {
-			database: Database;
-			userId: MongooseID;
-			sockerController: SocketController;
-		}
-	}
-	var Database:Database
-	var conv: ConversationController?
+  namespace Express {
+    interface Locals {
+      database: Database;
+      userId: MongooseID;
+      sockerController: SocketController;
+    }
+    interface Request {
+      currentUser?: IUser;
+    }
+  }
+
+  var Database: Database;
+  var conv: ConversationController?;
 }
